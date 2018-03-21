978e4913e53178742e053c8848b4948cfb9a860e
kmills <ksmills82@gmail.com>
2018-03-21 17:25:42 -0400

    started front-end header/nav for index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
-
+var currentTime = moment();
+var currentDate = moment(currentTime).format("hh:mm");
 
 var weatherAPIKey = "f3b57377fd45d3c75ef5eb8b659e8ad3";
-
 // Here we can set the var city to a value coming from the flight tracker API
 // We can associate the ajax call to an on-click so that the city info can populate
 var city = "";
-
+// "city" must be city,state format to populate URL correctly. Ex below for console.log
+city = "orlando,florida";
 // Here we are building the URL we need to query the database
 var weatherURL = "https://api.openweathermap.org/data/2.5/weather?" +
   "q=" + city +"&units=imperial&appid=" + weatherAPIKey;
+  console.log(weatherURL);
 
 // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
@@ -18,4 +20,5 @@ $.ajax({
   // We store all of the retrieved data inside of this "response"
   .then(function(response) {
       console.log(response);
+      console.log(currentDate)
   });
